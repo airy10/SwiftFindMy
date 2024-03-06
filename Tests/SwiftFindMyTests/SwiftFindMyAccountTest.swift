9205ba580ab576e43627ae7ede8d768172bf71a7
Airy André <airy.andre@free.fr>
2024-03-06 19:36:30 +0100

    Update crypto tests

--- Tests/SwiftFindMyTests/SwiftFindMyAccountTest.swift
+++ Tests/SwiftFindMyTests/SwiftFindMyAccountTest.swift
@@ -17,15 +17,38 @@ final class SwiftFindMyTestAccount: XCTestCase {
         // Put setup code here. This method is called b ≈efore the invocation of each test method in the class.
     }
 
-    func testEncryptPassword() async throws {
+    func testEncryptPassword() throws {
 
         let salt = try Base64.decode("+DCyn+hGpL/1bFXhzcnImThO5YNCFLFIJo5K9YxtdvI=")
         let password = "password"
         let iterations = 10
 
-        let res = encryptPassword(password: password, salt: salt, iterations: iterations)
+        let res = Crypto.encryptPassword(password: password, salt: salt, iterations: iterations)
 
         XCTAssertEqual(Base64.encode(res), "0UYD64eEgA8XmPYqcVup4SMcNXmxw3PmrCrWKXpmAco=")
+
+    }
+
+    func testCBCEncrypt() throws {
+
+        let sessionKey = [UInt8]("some session key".utf8)
+        let data = [UInt8]("bla bla this is some random test to encrypt".utf8)
+
+        let encrypted = Crypto.encryptSpdAesCbc(sessionKey: sessionKey, data: data)
+
+        XCTAssertEqual(Base64.encode(encrypted, 10000), "XmItwgjqUfPs+pEwfdMAqsRgJAwhVskab+aVHIwEehJFpP2/lMU21r/gwgD+S17E")
+
+    }
+
+    func testCBCDecrypt() throws {
+
+        let sessionKey = [UInt8]("some session key".utf8)
+        let data = [UInt8]("bla bla this is some random test to encrypt".utf8)
+        let encrypted = try Base64.decode("XmItwgjqUfPs+pEwfdMAqsRgJAwhVskab+aVHIwEehJFpP2/lMU21r/gwgD+S17E")
+
+        let decrypted = Crypto.decryptSpdAesCbc(sessionKey: sessionKey, data: encrypted)
+
+        XCTAssertEqual(decrypted, data)
 
     }
 
